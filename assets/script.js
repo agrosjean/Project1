7d3dc73f00d6671fcdfc2ab3124d96b8754ddecb
alexandre <agrosjean007@gmail.com>
2021-10-27 13:45:12 -0400

    Added images and titles category

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,47 @@
 // Establish Globale Variable / Elements
 const barType = document.getElementById('bar-type')
-const barImage = document.querySelector('.bar-image')
-const images = document.getElementsByTagName('img')
+const showInfo = document.getElementById('details')
+// const img = document.getElementsByTagName('img')
+// const barImage = document.querySelector('.bar-image')
+// const images = document.getElementsByTagName('img')
 
 
 
 
-// Fetch the API
-// fetch('http://localhost:3000/bars')
-//     .then(response => response.json())
-//     .then(bars => {
-//         bars.forEach(barObj => {
-//             addBarImage(barObj)
-//         })
-//     })
+// Fetch the API individually
+fetch('http://localhost:3000/jazz-bars')
+    .then(response => response.json())
+    .then(barsArray => {
+        barsArray.forEach(bar => {
+            const li = document.createElement('li')
+            li.textContent = bar.category
+            barType.appendChild(li)
+        })
+    })
 
 
-// Filtering the DB based on the Type of bars
-// const barCategory = bars.filter((bar) => {
-//     let barCat = 
-// })
+        // function handleBarClick(bar) {
+        //     let nameBar = document.createElement('name')
+        //     nameBar.textContent = bar.name
+        // }
 
 
 
 
 // Display img of the types of bars 
 
-function addBarImage(barObj) {
-    const img = document.createElement('img')
-    img.src = barObj.image
-    // if (barObj === "Jazz Bar")
-    // return 
-    barType.appendChild(img)
-    img.addEventListener('click', () => barInfo(barObj))
+// function addBarImage(barObj) {
+//     const img = document.createElement('img')
+//     img.src = barObj.image
+//     // if (barObj === "Jazz Bar")
+//     // return 
+//     barType.appendChild(img)
+//     img.addEventListener('click', () => barInfo(barObj))
 
-}
-
-
+// }
 
 
-function barInfo() {
 
-}
 
+// function barInfo() {
 
