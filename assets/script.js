9e6933c74a3158f87e35d74032d717c932ce4772
alexandre <agrosjean007@gmail.com>
2021-10-27 14:18:49 -0400

    Handling Jazz bars

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,28 @@
 // Establish Globale Variable / Elements
-const barType = document.getElementById('bar-type')
+const jazzBar = document.getElementById('jazz-bar')
 const showInfo = document.getElementById('details')
-// const img = document.getElementsByTagName('img')
-// const barImage = document.querySelector('.bar-image')
-// const images = document.getElementsByTagName('img')
 
 
-
+jazzBar.addEventListener('click', handleJazzClick)
 
 // Fetch the API individually
-fetch('http://localhost:3000/jazz-bars')
-    .then(response => response.json())
-    .then(barsArray => {
-        barsArray.forEach(bar => {
-            const li = document.createElement('li')
-            li.textContent = bar.category
-            barType.appendChild(li)
+
+
+function handleJazzClick() {
+    fetch('http://localhost:3000/jazz')
+        .then(response => response.json())
+        .then(barsArray => {
+            barsArray.forEach(bar => {
+                const nameJ = document.createElement('p')
+                nameJ.textContent = bar.name
+                const addressJ = document.createElement('p')
+                addressJ.textContent = bar.address
+
+                jazzBar.append(nameJ, addressJ)
+
+            })
         })
-    })
-
-
-        // function handleBarClick(bar) {
-        //     let nameBar = document.createElement('name')
-        //     nameBar.textContent = bar.name
-        // }
+}
 
 
 
